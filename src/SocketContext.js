804de45d6e6c95836fe283f5e45c699839d8d616
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2024-01-24 17:35:46 +0530

    the video and audio API is working as expected

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -5,15 +5,16 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('http://192.168.0.24:5000');
 
-const ContextProvider = ({ children}) =>{
+const ContextProvider = ({ children }) =>{
 
-    const [stream , setStream] =useState(null);
+    const [stream , setStream] =useState();
     const [me , setMe] =useState('');
     const [call , setCall] =useState({});
     const [callAccepted , setCallAccepted] =useState(false);
     const [callEnded , setCallEnded] =useState(false);
+    const [name , setName] =useState("");
 
     const myVideo = useRef();
     const userVideo = useRef();
@@ -27,6 +28,7 @@ const ContextProvider = ({ children}) =>{
         })
 
         socket.on('me',(id)=>setMe(id));
+
         socket.on('calluser', ({from , name: callerName , signal})=>{
             setCall({ isReceivedCall : true , from , name: callerName, signal})
         })
@@ -46,15 +48,55 @@ const ContextProvider = ({ children}) =>{
         })
 
         peer.signal(call.signal);
-        
+
         connectionRef.current=peer;
     }
 
-    const callUser = () =>{
+    const callUser = (id) =>{
+        const peer = new Peer({ initiator:false, trickle:true , stream });
 
+        peer.on('signal' , (data)=>{
+            socket.emit('calluser' , {userToCall:id , signalData: data,from:me, name});
+        }) ;
+
+        peer.on('stream',(currentStream)=>{
+            userVideo.current.srcObject = currentStream
+        })
+
+        socket.on('callaccepted',(signal)=>{
+            setCallAccepted(true);
+
+            peer.signal(signal);
+        })
+
+        connectionRef.current = peer;
     }
 
     const leaveCall = ()=>{
+        setCallEnded(true);
+        connectionRef.current.destroy();
 
+        window.location.reload();
     }
+
+    return(
+        <SocketContext.Provider value={{
+            call,
+            callAccepted,
+            myVideo,
+            userVideo,
+            stream,
+            name,
+            setName,
+            callEnded,
+            me,
+            callUser,
+            leaveCall,
+            answerCall,
+        }}>
+            {children}
+        </SocketContext.Provider>
+    )
 }
+
+export {ContextProvider , SocketContext};
